209ce7d365b7948eca4d23ca8e9832154da107d2
Anton Pravdin <antonpravdin@gmail.com>
2016-12-08 15:48:45 -0500

    Fixes up importing for tests

--- tests/test_heap.py
+++ tests/test_heap.py
@@ -1,5 +1,5 @@
 import unittest
-import heap
+from heap import heap
 
 SORTED_HEAP = [3, 2, 1]
 MAX = 999
@@ -11,6 +11,8 @@ class TestHeapq(unittest.TestCase):
     def setUp(self):
         self.h = heap.heapq()
         self.h.heapify(BASIC_HEAP)
+        self.n1 = heap.heap_node(MIN)
+        self.n2 = heap.heap_node(MAX)
 
     def test_root(self):
         self.assertEqual(self.h.root.val, MAX)
@@ -23,6 +25,25 @@ class TestHeapq(unittest.TestCase):
 
             for child in node.children:
                 self.assertGreater(node, child)
+
+    def test_node(self):
+        self.assertEqual(self.n1.val, MIN)
+
+    def test_node_add_child(self):
+        self.n2.add_child(self.n1)
+
+        # Children set correctly
+        self.assertIn(self.n1, self.n2.children)
+        self.assertEqual(len(self.n2.children), 1)
+
+        # Parent set correctly
+        self.assertEqual(self.n1.parent, self.n2)
+
+    def test_node_lt(self):
+        self.assertLess(self.n1, self.n2)
+
+    def test_node_gt(self):
+        self.assertGreater(self.n2, self.n1)
 
 
 if __name__ == '__main__':
